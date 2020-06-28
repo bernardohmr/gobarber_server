d016d856d0182d91cd4f3c86494e0e4648f42262
Bernardo Rodrigues <bernardohmr@gmail.com>
2020-06-28 10:42:54 -0300

    initial

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'b478b93fe2679507ae30ba7d489d1ae9',
+    expiresIn: '1d',
+  },
+};
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata';
+
+import express from 'express';
+import routes from './routes';
+
+import './database';
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333!');
+});

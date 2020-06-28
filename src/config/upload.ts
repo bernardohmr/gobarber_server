2dc30e4e029de60c98f0e5e8b259f839c0050cbf
Bernardo Rodrigues <bernardohmr@gmail.com>
2020-06-28 12:31:58 -0300

    Errors class global handler

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,5 +1,19 @@
+import path from 'path';
+import crypto from 'crypto';
 import multer from 'multer';
 
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
 export default {
-  storage: multer.diskStorage({}),
+  directory: tmpFolder, // not a multer config
+
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
 };
